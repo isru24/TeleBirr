eef7eb5ce5aa889babfe78dadf4dc64f3c97e089
isru24 <israelmandefro165@gmail.com>
2025-08-24 13:55:09 +0300

    modified:   lib/main.dart 	renamed:    lib/pages/Loginpage.dart -> lib/screens/Loginpage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:telebirr/pages/Loginpage.dart';
+import 'package:telebirr/screens/Loginpage.dart';
 import 'package:telebirr/screens/home_screen.dart';
 
 void main() {
